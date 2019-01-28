612f71a141d742f2848708138575d3d0e5a4bff3
Trung Pham <phambaotrung@gmail.com>
2019-01-28 01:30:47 -0800

    Revert 5de0d7d

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
-#include <sodium.h>
-
 #include "argon2/argon2.h"
 #include "hc256/hc256.h"
 #include "skein3fish/skeinApi.h"
 #include "skein3fish/threefishApi.h"
+#include "randombytes/randombytes.h"
 
-#define STB_LIB_IMPLEMENTATION
-#include "stb_lib.h"
+#define ZPL_IMPLEMENTATION
+#include "zpl.h"
 
 #define KEY_LEN 256UL
 #define HEADER_LEN 6UL
@@ -15,7 +14,8 @@
 #define HC256_LEN 64UL
 #define T3F_KEY_LEN 128UL
 #define T3F_BLOCK_LEN 128UL
-#define CHUNK_LEN (T3F_BLOCK_LEN)
+#define NUM_BLOCKS 1024UL
+#define CHUNK_LEN (T3F_BLOCK_LEN * NUM_BLOCKS)
 #define SKEIN_MAC_LEN 64UL
 #define ENC_KEY_LEN (HC256_LEN + T3F_KEY_LEN + TWEAK_LEN + SKEIN_MAC_LEN)
 
@@ -36,19 +36,15 @@ void check_fatal_err(int cond, char *msg) {
     }
 }
 
-FILE *t3fc_fopen(const char *path, const char *flags) {
-    FILE *f = stb__fopen(path, flags);
-    check_fatal_err(f == NULL, "cannot open file.");
-    return f;
-}
-
 void get_key_from_file(const char *key_file, uint8_t *key) {
-    FILE *f = t3fc_fopen(key_file, "rb");
-    check_fatal_err(stb_filelen(f) != KEY_LEN,
+    zpl_file f = {0};
+    zplFileError rc = zpl_file_open(&f, key_file);
+    check_fatal_err(rc != ZPL_FILE_ERROR_NONE, "cannot open file.");
+    check_fatal_err(zpl_file_size(&f) != KEY_LEN,
                     "key file must have exactly 256 bytes.");
-    check_fatal_err(fread(key, 1, KEY_LEN, f) != KEY_LEN,
+    check_fatal_err(zpl_file_read(&f, key, KEY_LEN) != 1,
                     "cannot read key from file.");
-    fclose(f);
+    zpl_file_close(&f);
 }
 
 void *t3fc_malloc(size_t s) {
@@ -57,25 +53,27 @@ void *t3fc_malloc(size_t s) {
     return buf;
 }
 
-void encrypt(FILE *input, FILE *output, uint8_t *key);
-void encrypt_chunk(unsigned char *chunk, size_t nread, FILE *input,
-                   FILE *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
-                   SkeinCtx_t *skein_x);
-void decrypt(FILE *input, FILE *output, uint8_t *key);
-void decrypt_chunk(unsigned char *chunk, size_t nread, FILE *input,
-                   FILE *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
-                   SkeinCtx_t *skein_x);
+void encrypt(zpl_file *input, zpl_file *output, uint8_t *key);
+void encrypt_chunk(unsigned char *chunk, size_t nread, zpl_file *input,
+                   zpl_file *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
+                   SkeinCtx_t *skein_x, size_t total_bytes_read);
+void decrypt(zpl_file *input, zpl_file *output, uint8_t *key);
+void decrypt_chunk(unsigned char *chunk, size_t nread, zpl_file *input,
+                   zpl_file *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
+                   SkeinCtx_t *skein_x, size_t total_bytes_read);
 void prepare(uint8_t *enc_key, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
              SkeinCtx_t *skein_x);
 
 int main(int argc, char *argv[]) {
 
-    check_fatal_err(sodium_init() < 0, "cannot initialize libsodium.");
-
-    if (argc == 2 && strcmp(argv[1], "-mk") == 0) {
-        randombytes_buf(key, KEY_LEN);
-        check_fatal_err(fwrite(key, 1, KEY_LEN, stdout) != KEY_LEN,
-                        "cannot write key to file.");
+    if (argc == 3 && strcmp(argv[1], "-mk") == 0) {
+        randombytes(key, KEY_LEN);
+        zpl_file f = {0};
+        zplFileError rc = zpl_file_create(&f, argv[2]);
+        check_fatal_err(rc != ZPL_FILE_ERROR_NONE, "cannot create file.");
+        check_fatal_err(zpl_file_write(&f, key, KEY_LEN) != 1,
+                    "cannot write key to file.");
+        zpl_file_close(&    f);
 
     } else if (argc == 8 &&
                (strcmp(argv[1], "-e") == 0 || strcmp(argv[1], "-d") == 0) &&
@@ -83,7 +81,7 @@ int main(int argc, char *argv[]) {
                strcmp(argv[6], "-o") == 0) {
 
         get_key_from_file(argv[3], key);
-        if (stb_fexists(argv[7])) {
+        if (zpl_file_exists(argv[7])) {
             char yn;
             do {
                 printf("File exists. Do you want to overwrite? (y/n) ");
@@ -94,15 +92,19 @@ int main(int argc, char *argv[]) {
                 return EXIT_SUCCESS;
             }
         }
-        FILE *input = t3fc_fopen(argv[5], "rb");
-        FILE *output = t3fc_fopen(argv[7], "wb");
+        zpl_file input = {0};
+        zpl_file output = {0};
+        zplFileError rc = zpl_file_open(&input, argv[5]);
+        check_fatal_err(rc != ZPL_FILE_ERROR_NONE, "cannot open file.");
+        rc = zpl_file_create(&output, argv[7]);
+        check_fatal_err(rc != ZPL_FILE_ERROR_NONE, "cannot create file.");
         if (strcmp(argv[1], "-e") == 0) {
-            encrypt(input, output, key);
+            encrypt(&input, &output, key);
         } else if (strcmp(argv[1], "-d") == 0) {
-            decrypt(input, output, key);
+            decrypt(&input, &output, key);
         }
-        fclose(input);
-        fclose(output);
+        zpl_file_close(&input);
+        zpl_file_close(&output);
 
     } else {
         check_fatal_err(1, "unknown options.");
@@ -132,14 +134,15 @@ void prepare(uint8_t *enc_key, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
     skeinMacInit(skein_x, skein_mac_key, SKEIN_MAC_LEN, Skein512);
 }
 
-void encrypt(FILE *input, FILE *output, uint8_t *key) {
+void encrypt(zpl_file *input, zpl_file *output, uint8_t *key) {
 
-    check_fatal_err(fwrite(header, 1, HEADER_LEN, output) != HEADER_LEN,
-                    "cannot write header.");
+    isize bytes_written = HEADER_LEN;
+    b32 rc = zpl_file_write_at_check(output, header, HEADER_LEN, 0, &bytes_written);
+    check_fatal_err(rc != 1 || bytes_written != HEADER_LEN, "cannot write header.");
     uint8_t salt[SALT_LEN];
-    randombytes_buf(salt, SALT_LEN);
-    check_fatal_err(fwrite(salt, 1, SALT_LEN, output) != SALT_LEN,
-                    "cannot write salt.");
+    randombytes(salt, SALT_LEN);
+    rc = zpl_file_write_at_check(output, salt, SALT_LEN, HEADER_LEN, &bytes_written);
+    check_fatal_err(rc != 1 || bytes_written != SALT_LEN, "cannot write salt.");
 
     uint8_t enc_key[ENC_KEY_LEN];
     check_fatal_err(argon2id_hash_raw(T, M, P, key, KEY_LEN, salt, SALT_LEN,
@@ -154,22 +157,22 @@ void encrypt(FILE *input, FILE *output, uint8_t *key) {
     skeinUpdate(skein_x, salt, SALT_LEN);
 
     unsigned char *chunk = t3fc_malloc(CHUNK_LEN);
-    size_t read_len = 0;
+    size_t bytes_read = 0;
+    size_t total_bytes_read = 0;
     while (1) {
-        read_len = fread(chunk, 1, CHUNK_LEN, input);
-        check_fatal_err(read_len != CHUNK_LEN && ferror(input),
-                        "cannot read input.");
-        encrypt_chunk(chunk, read_len, input, output, hc256_ctx, t3f_x,
-                      skein_x);
-        if (read_len < CHUNK_LEN) {
+        rc = zpl_file_read_at_check(input, chunk, CHUNK_LEN, total_bytes_read, &bytes_read);
+        check_fatal_err(rc != 1, "cannot read input.");
+        encrypt_chunk(chunk, bytes_read, input, output, hc256_ctx, t3f_x, skein_x, total_bytes_read);
+        total_bytes_read += bytes_read;
+        if (bytes_read < CHUNK_LEN) {
             break;
         }
     }
 
     unsigned char hash[SKEIN_MAC_LEN];
     skeinFinal(skein_x, hash);
-    check_fatal_err(fwrite(hash, 1, SKEIN_MAC_LEN, output) != SKEIN_MAC_LEN,
-                    "cannot write Skein MAC.");
+    rc = zpl_file_write_at_check(output, hash, SKEIN_MAC_LEN, HEADER_LEN + SALT_LEN + total_bytes_read, &bytes_written);
+    check_fatal_err(rc != 1 || bytes_written != SKEIN_MAC_LEN, "cannot write Skein MAC.");
 
     free(hc256_ctx);
     free(t3f_x);
@@ -177,16 +180,17 @@ void encrypt(FILE *input, FILE *output, uint8_t *key) {
     free(skein_x);
 }
 
-void decrypt(FILE *input, FILE *output, uint8_t *key) {
+void decrypt(zpl_file *input, zpl_file *output, uint8_t *key) {
 
     uint8_t in_header[HEADER_LEN];
-    check_fatal_err(fread(in_header, 1, HEADER_LEN, input) != HEADER_LEN,
-                    "cannot read header.");
+    isize bytes_read = 0;
+    b32 rc = zpl_file_read_at_check(input, in_header, HEADER_LEN, 0, &bytes_read);
+    check_fatal_err(rc != 1 || bytes_read != HEADER_LEN, "cannot read header.");
     check_fatal_err(memcmp(in_header, header, HEADER_LEN) != 0,
                     "wrong header.");
     uint8_t salt[SALT_LEN];
-    check_fatal_err(fread(salt, 1, SALT_LEN, input) != SALT_LEN,
-                    "cannot read salt.");
+    rc = zpl_file_read_at_check(input, salt, SALT_LEN, HEADER_LEN, &bytes_read);
+    check_fatal_err(rc != 1 || bytes_read != SALT_LEN, "cannot read salt.");
 
     uint8_t enc_key[ENC_KEY_LEN];
     check_fatal_err(argon2id_hash_raw(T, M, P, key, KEY_LEN, salt, SALT_LEN,
@@ -201,25 +205,25 @@ void decrypt(FILE *input, FILE *output, uint8_t *key) {
     skeinUpdate(skein_x, salt, SALT_LEN);
 
     size_t infile_len =
-        stb_filelen(input) - (HEADER_LEN + SALT_LEN + SKEIN_MAC_LEN);
+        zpl_file_size(input) - (HEADER_LEN + SALT_LEN + SKEIN_MAC_LEN);
     unsigned char *chunk = t3fc_malloc(CHUNK_LEN);
-    size_t read_len = 0;
     size_t chunk_len = CHUNK_LEN;
     size_t num_read = infile_len / CHUNK_LEN + (infile_len % CHUNK_LEN != 0);
+    size_t total_bytes_read = HEADER_LEN + SALT_LEN;
     for (size_t i = 0; i < num_read; ++i) {
         if (i == num_read - 1) {
             chunk_len = infile_len - i * CHUNK_LEN;
         }
-        read_len = fread(chunk, 1, chunk_len, input);
-        check_fatal_err(read_len != chunk_len && ferror(input),
-                        "cannot read input.");
-        decrypt_chunk(chunk, read_len, input, output, hc256_ctx, t3f_x,
-                      skein_x);
+        rc = zpl_file_read_at_check(input, chunk, chunk_len, total_bytes_read, &bytes_read);
+        check_fatal_err(rc != 1, "cannot read input.");
+        decrypt_chunk(chunk, bytes_read, input, output, hc256_ctx, t3f_x,
+                      skein_x, total_bytes_read - (HEADER_LEN + SALT_LEN));
+        total_bytes_read += bytes_read;
     }
 
-    read_len = fread(chunk, 1, SKEIN_MAC_LEN, input);
-    check_fatal_err(read_len != SKEIN_MAC_LEN && ferror(input),
-                    "cannot read input.");
+    rc = zpl_file_read_at_check(input, chunk, SKEIN_MAC_LEN, total_bytes_read, &bytes_read);
+    check_fatal_err(rc != 1 || bytes_read != SKEIN_MAC_LEN, "cannot read input.");
+
     unsigned char hash[SKEIN_MAC_LEN];
     skeinFinal(skein_x, hash);
     check_fatal_err(memcmp(hash, chunk, SKEIN_MAC_LEN) != 0,
@@ -231,25 +235,27 @@ void decrypt(FILE *input, FILE *output, uint8_t *key) {
     free(skein_x);
 }
 
-void decrypt_chunk(unsigned char *chunk, size_t nread, FILE *input,
-                   FILE *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
-                   SkeinCtx_t *skein_x) {
+void decrypt_chunk(unsigned char *chunk, size_t nread, zpl_file *input,
+                   zpl_file *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
+                   SkeinCtx_t *skein_x, size_t total_bytes_read) {
     unsigned char tmp[T3F_BLOCK_LEN];
     unsigned char ctr[T3F_BLOCK_LEN];
     unsigned char tmp_out[T3F_BLOCK_LEN];
 
     uint32_t i = 0;
     size_t in_len = nread;
+    isize bytes_written = 0;
+    isize offset = total_bytes_read;
+    b32 rc = 0;
     for (; in_len >= T3F_BLOCK_LEN; ++i, in_len -= T3F_BLOCK_LEN) {
         hc256_gen_bytes(hc256_ctx, ctr, T3F_BLOCK_LEN);
         threefishEncryptBlockBytes(t3f_x, ctr, tmp);
         for (uint32_t j = 0; j < T3F_BLOCK_LEN; ++j) {
             tmp_out[j] = tmp[j] ^ chunk[i * T3F_BLOCK_LEN + j];
         }
-        skeinUpdate(skein_x, &chunk[i * T3F_BLOCK_LEN], T3F_BLOCK_LEN);
-        check_fatal_err(fwrite(tmp_out, 1, T3F_BLOCK_LEN, output) !=
-                            T3F_BLOCK_LEN,
-                        "cannot write to file.");
+        rc = zpl_file_write_at_check(output, tmp_out, T3F_BLOCK_LEN, offset, &bytes_written);
+        check_fatal_err(rc != 1 || bytes_written != T3F_BLOCK_LEN, "cannot write to file.");
+        offset += T3F_BLOCK_LEN;
     }
     if (in_len > 0) {
         hc256_gen_bytes(hc256_ctx, ctr, T3F_BLOCK_LEN);
@@ -257,21 +263,24 @@ void decrypt_chunk(unsigned char *chunk, size_t nread, FILE *input,
         for (uint32_t j = 0; j < in_len; ++j) {
             tmp_out[j] = tmp[j] ^ chunk[i * T3F_BLOCK_LEN + j];
         }
-        skeinUpdate(skein_x, &chunk[i * T3F_BLOCK_LEN], in_len);
-        check_fatal_err(fwrite(tmp_out, 1, in_len, output) != in_len,
-                        "cannot write to file.");
+        rc = zpl_file_write_at_check(output, tmp_out, in_len, offset, &bytes_written);
+        check_fatal_err(rc != 1 || bytes_written != in_len, "cannot write to file.");
     }
+    skeinUpdate(skein_x, chunk, nread);
 }
 
-void encrypt_chunk(unsigned char *chunk, size_t nread, FILE *input,
-                   FILE *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
-                   SkeinCtx_t *skein_x) {
+void encrypt_chunk(unsigned char *chunk, size_t nread, zpl_file *input,
+                   zpl_file *output, hc256_ctx_t *hc256_ctx, ThreefishKey_t *t3f_x,
+                   SkeinCtx_t *skein_x, size_t total_bytes_read) {
     unsigned char tmp[T3F_BLOCK_LEN];
     unsigned char ctr[T3F_BLOCK_LEN];
     unsigned char tmp_out[T3F_BLOCK_LEN];
 
     uint32_t i = 0;
+    isize bytes_written = 0;
+    isize offset = HEADER_LEN + SALT_LEN + total_bytes_read;
     size_t in_len = nread;
+    b32 rc = 0;
     for (; in_len >= T3F_BLOCK_LEN; ++i, in_len -= T3F_BLOCK_LEN) {
         hc256_gen_bytes(hc256_ctx, ctr, T3F_BLOCK_LEN);
         threefishEncryptBlockBytes(t3f_x, ctr, tmp);
@@ -279,9 +288,9 @@ void encrypt_chunk(unsigned char *chunk, size_t nread, FILE *input,
             tmp_out[j] = tmp[j] ^ chunk[i * T3F_BLOCK_LEN + j];
         }
         skeinUpdate(skein_x, tmp_out, T3F_BLOCK_LEN);
-        check_fatal_err(fwrite(tmp_out, 1, T3F_BLOCK_LEN, output) !=
-                            T3F_BLOCK_LEN,
-                        "cannot write to file.");
+        rc = zpl_file_write_at_check(output, tmp_out, T3F_BLOCK_LEN, offset, &bytes_written);
+        check_fatal_err(rc != 1 || bytes_written != T3F_BLOCK_LEN, "cannot write to file.");
+        offset += T3F_BLOCK_LEN;
     }
     if (in_len > 0) {
         hc256_gen_bytes(hc256_ctx, ctr, T3F_BLOCK_LEN);
@@ -290,7 +299,7 @@ void encrypt_chunk(unsigned char *chunk, size_t nread, FILE *input,
             tmp_out[j] = tmp[j] ^ chunk[i * T3F_BLOCK_LEN + j];
         }
         skeinUpdate(skein_x, tmp_out, in_len);
-        check_fatal_err(fwrite(tmp_out, 1, in_len, output) != in_len,
-                        "cannot write to file.");
+        rc = zpl_file_write_at_check(output, tmp_out, in_len, offset, &bytes_written);
+        check_fatal_err(rc != 1 || bytes_written != in_len, "cannot write to file.");
     }
 }
